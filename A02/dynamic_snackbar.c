552c71d3d4a8c6710500b34c9b2d1a8aefa2cb58
Joon Luther <joonluther@wc124-dhcp-68.brynmawr.edu>
2022-02-03 12:29:12 -0500

    dynamic snackbar and repeat

--- A02/dynamic_snackbar.c
+++ A02/dynamic_snackbar.c
@@ -9,5 +9,30 @@ struct snack {
 };
 
 int main() {
+
+    int numSnacks;
+
+    printf("Enter a number of snacks: ");
+    scanf("%d", &numSnacks);
+    struct snack snacks[numSnacks];
+
+    for(int i = 0; i < numSnacks; i++) {
+
+      printf("Enter a name: ");
+      scanf("%s", snacks[i].name);
+      printf("Enter a cost: ");
+      scanf("%f", &snacks[i].cost);
+      printf("Enter a quantity: ");
+      scanf("%d", &snacks[i].quantity);
+
+    }
+
+    for(int i = 0; i < numSnacks; i++) {
+
+      printf("%d) name: %s, cost: %f, quantity: %d\n", i+1, snacks[i].name, snacks[i].cost, snacks[i].quantity);
+
+    }
+
   return 0;
+
 }
--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void init_array(char *arr, int size, char str[], int str_length) {
+    int i;
+    for (i = 0; i < size; i++) {
+        arr[i] = (str[i % str_length]);
+    }
+}
 
 int main() {
+
+  char str1[33];
+  int len;
+  char *p;
+  int totalSize;
+  int repeat;
+
+  printf("Enter a word: \n");
+  scanf("%s", str1);
+  printf("Enter a word count: \n");
+  scanf("%d", &repeat);
+  len = (strlen(str1)*repeat);
+  printf("%d", len);
+
+  p = malloc(sizeof(char)*len);
+  if (p == NULL) {
+        printf("malloc error\n");
+        exit(1);
+  }
+  init_array(p,len,str1,strlen(str1));
+
+  for(int i = 0; i < len*repeat; i++) {
+    printf("%c", p[i]);
+  }
+
+  printf("\n");
+
+  free(p);
+  p = NULL;
   return 0;
 }
