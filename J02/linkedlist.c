c19b6450aec19172f8a99f340fad1254f7dbab6d
Joon Luther <joonluther@wc124-dhcp-68.brynmawr.edu>
2022-02-24 14:57:19 -0500

    J02 Linked List

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -15,17 +15,37 @@ struct cake makeCake(const char* flavor, float cost) {
   return newCake;
 }
 
+struct cake cheapestCake(struct cake head) {
+  struct cake cheapest = head;
+  struct cake curr = head;
+  printf("cake: %s ($%f)\n", curr.flavor, curr.cost);
+  while (curr.next != NULL) {
+    curr = *curr.next;
+    printf("cake: %s ($%f)\n", curr.flavor, curr.cost);
+    if(curr.cost < cheapest.cost) {
+      cheapest = curr;
+    }
+  }
+
+  return cheapest;
+
+}
+
 // todo: implement cheapestCake
 
 int main() {
   struct cake cake1 = makeCake("red velvet", 2.00);
   struct cake cake2 = makeCake("chocolate", 1.75);
   struct cake cake3 = makeCake("mocha", 3.50);
+  struct cake cake4 = makeCake("peach", 20.00);
   // todo: define 4th cake
 
   cake1.next = &cake2;
   cake2.next = &cake3;
+  cake3.next = &cake4;
+
   // draw stack and heap here
 
   // todo: call cheapestCake
+  printf("cheapest cake: %s\n", cheapestCake(cake1).flavor);
 }
