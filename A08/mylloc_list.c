cbe1e292e4f0df4c588b30a0044e956a6045b262
Joon Luther <joonluther@wc124-dhcp-68.brynmawr.edu>
2022-04-03 21:23:15 -0400

    A08

--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -4,6 +4,7 @@
 
 struct chunk {
   int size;
+  int in_use;
   struct chunk *next;
 };
 struct chunk *flist = NULL;
@@ -13,19 +14,98 @@ void *malloc (size_t size) {
     return NULL;
   }
 
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  while(next != NULL) {
+	  if(next->size >=size) {
+		  next->in_use = size;
+		  if(prev != NULL) {
+			  prev->next = next->next;
+		  }  else {
+			  flist = next->next;
+		  }
+		  return (void*)(next + 1);
+	  } else {
+		  prev = next;
+		  next = next->next;
+	  }
+  }
+	
   void *memory = sbrk(size + sizeof(struct chunk));
   if (memory == (void *) -1) {
     return NULL;
   } else {
-    struct chunk* cnk = (struct chunk*) memory;
+    struct chunk* cnk = (struct chunk*)memory;
+    cnk->size = size;
+    cnk->in_use = size;
     return (void*) (cnk + 1);
   }
 }
 
 void free(void *memory) {
+  if(memory != NULL) {
+	  struct chunk *cnk = (struct chunk*)((struct chunk*)memory - 1);
+	  cnk->in_use = 0;
+	  cnk->next = flist;
+	  flist = cnk;
+  }
   return;
 }
 
 void fragstats(void* buffers[], int len) {
-}
+	int totalBlocks = 0;
+	int totalFree = 0;
+	int totalUsed = 0;
+	int idx = 0;
+	while(buffers[idx] == NULL) {
+		idx++;
+	}
+	struct chunk *first = (struct chunk*)((struct chunk*)(buffers[idx])-1);
+	int intUnused = 0;
+	int intMin = first->size;
+	int intMax = first->size;
+	struct chunk *curr = flist;
+	int extUnused = 0;
+	int extMin, extMax;
 
+	if(curr != NULL) {
+		extMin = curr->size;
+		extMax = curr->size;
+	}
+	
+	while(curr != NULL) {
+		totalFree++;
+		//printf("size of free: %d\n", curr->size);
+		extUnused += curr->size;
+		if(curr->size > extMax) {
+			extMax = curr->size;
+		}
+		if(curr->size < extMin) {
+			extMin = curr->size;
+		}
+		curr = curr->next;
+	}
+
+	for(int i = 0; i < len; i++) {
+		if(buffers[i] != NULL) {
+			struct chunk *cnk = (struct chunk*)((struct chunk*)(buffers[i])-1);
+			//printf("idx: %d, size: %d, in use: %d\n", i, cnk->size, cnk->in_use);
+			totalUsed++;
+			intUnused += (cnk->size)-(cnk->in_use);
+
+			if(cnk->size > intMax) {
+				intMax = cnk->size;
+			}
+			if(cnk->size < intMin) {
+				intMin = cnk->size;
+			}
+		}
+	}
+
+	totalBlocks = totalUsed + totalFree;
+	printf("Total blocks: %d Free: %d Used: %d\n", totalBlocks, totalFree, totalUsed);
+	printf("Internal unused: total: %d average: %f smallest: %d largest: %d\n", intUnused, (float)(intUnused/len), intMin, intMax);
+	printf("External unused: total: %d average: %f smallest: %d largest: %d\n", extUnused, (float)(extUnused/totalFree), extMin, extMax);
+
+}
--- A08/reallocate.c
+++ A08/reallocate.c
@@ -5,6 +5,24 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
+	void *init = sbrk(0);
+  	void *current;
+ 	printf("The initial top of the heap is %p.\n", init);
+	
+       	for(int i = 0; i < 10; i++) {
+		int *memory = NULL;
+		memory = malloc(100);
+		if (memory == NULL) {
+        		fprintf(stderr, "malloc failed\n");
+       			return(1);
+      		}
+		*memory = 246;
+		free(memory);
+	}	
 
-  return 0 ;
+	current = sbrk(0);
+	int difference = (int) ((current - init));
+	printf("The current top of the heap is %p.\n", current);
+	printf("Difference of %d (0x%x) bytes\n", difference, difference);
+       	return 0 ;
 }
